9293d6d6a742368ed241e8848652db8100966b73
armaBurton <armanarma@armaburton.com>
2022-02-02 08:51:13 -0800

    pushing up test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Fast Food Ordering Tool', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Food Ordering/i);
   expect(linkElement).toBeInTheDocument();
 });
