17e4e83775e000753a252e5cd5c464d1986ba1ca
armaBurton <armanarma@armaburton.com>
2022-02-01 18:18:25 -0800

    dropdowns have been added, names and images change with dropdown changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import './App.css';
+import { useState } from 'react';
+import OrderImages from './OrderImages';
+import React from 'react';
+import FoodDropdown from './FoodDropdown';
+import DrinkDropdown from './DrinkDropdown';
+import SideDropdown from './SideDropdown';
 
 function App() {
-  return <></>;
+  const [foodId, setFoodId] = useState(1);
+  const foodArr = [`Burger`, `Ramen`, `Pizza`];
+
+  const [drinkId, setDrinkId] = useState(1);
+  const drinkArr = [`Whiskey`, `Beer`, `Milkshake`];
+
+  const [sideId, setSideId] = useState(1);
+  const sideArr = [`Cake`, `Fries`, `Salad`];
+
+  return <div className='app'>
+    <h1 className='FF-title'>Fast Food Ordering Tool</h1>
+    <OrderImages 
+      foodId={foodId}
+      foodArr={foodArr}
+      drinkId={drinkId}
+      drinkArr={drinkArr}
+      sideId={sideId}
+      sideArr={sideArr} 
+    />
+
+    <section className='dropdown-section'>
+      <div>
+        <FoodDropdown setFoodId={setFoodId} foodArr={foodArr} />
+      </div>
+      <div>
+        <DrinkDropdown setDrinkId={setDrinkId} drinkArr={drinkArr} />
+      </div>
+      <div>
+        <SideDropdown setSideId={setSideId} sideArr={sideArr} />
+      </div>
+    </section>
+  </div>;
 }
 
 export default App;
---
+++ src/DrinkDropdown.js
@@ -0,0 +1,13 @@
+import RenderDropdownItem from './RenderDropdownItem';
+
+export default function DrinkDropdown(props){
+
+  return <>
+    Select a Drink Item:&nbsp;
+    <select onChange={(e) => {props.setDrinkId(e.target.value);}}>
+      {
+        props.drinkArr.map((item, i) => <RenderDropdownItem key={`${item}${i}]`} item={item} i={i} />)
+      }
+    </select>;
+  </>;
+}
